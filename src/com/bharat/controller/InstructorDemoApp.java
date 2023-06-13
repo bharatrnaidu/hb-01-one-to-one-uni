978adebc5f6d15993314d2064be5e6987688abeb
bharatrnaidu <bharatrnaidu@gmail.com>
2023-06-13 16:40:58 +0530

    committing save instructor with details file into repo

--- src/com/bharat/controller/InstructorDemoApp.java
+++ src/com/bharat/controller/InstructorDemoApp.java
@@ -1,10 +1,55 @@
 package com.bharat.controller;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.bharat.entity.Instructor;
+import com.bharat.entity.InstructorDetail;
+import com.bharat.entity.Student;
+
 public class InstructorDemoApp {
 
     public static void main(String[] args)
     {
+	SessionFactory factory = new Configuration()
+                                    		.configure("hibernate.cfg.xml")
+                                    		.addAnnotatedClass(Instructor.class)
+                                    		.addAnnotatedClass(InstructorDetail.class)
+                                    		.buildSessionFactory();
 	
+	Session session = factory.getCurrentSession();
+	
+	try 
+	{
+	    // Create the objects.
+	    Instructor tempInstructor = new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+	    
+	    InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.madhuplayer.com/youtube", "Guitar");
+	    
+	    // associate the objects.
+	    tempInstructor.setInstructorDetail(tempInstructorDetail);
+	    
+	    // start a transaction.
+	    session.beginTransaction();
+		
+	    // save instructor.
+	    /*
+	     * Note: It will also save InstructorDetail (details object)
+	     * 		because of CascadeType.All
+	     */
+	    System.out.println("Saving Instructor: " + tempInstructor);
+	    session.save(tempInstructor);
+		
+	    // commit transaction
+	    session.getTransaction().commit();
+	    System.out.println("Done!");
+		
+	}
+	finally 
+	{
+		factory.close();
+	}
     }
 
 }
